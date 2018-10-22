bd34dfc2b26fb3e513421120a6c310abef3a6ef2
Kapil Goyal <kapil.goyal61@gmail.com>
2018-10-23 03:10:58 +0530

    DesDoc added UP04

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -45,7 +45,7 @@ process_execute (const char *file_name)
   strlcpy (fn_copy, file_name, PGSIZE);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_MAX, start_process, fn_copy);
+  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
